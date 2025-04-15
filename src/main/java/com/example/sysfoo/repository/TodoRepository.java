f2a28e879a6a7439c275d50da52806a6d8042cf1
gauravo7 <gaurav@o7services.com>
2025-04-15 09:42:09 +0530

    New Pro

--- src/main/java/com/example/sysfoo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sysfoo.repository;
-
-import com.example.sysfoo.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
